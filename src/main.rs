2964cfda6f94286b42243f64c724b05b93c24fd9
Pedro Mendes <pedrolopesme@gmail.com>
2026-03-21 21:27:48 -0300

    feat: add networking

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::{thread, time::Duration};
 
-use sysinfo::System;
+use std::collections::HashMap;
+use sysinfo::{Networks, System};
+
+struct NetworkState {
+    prev_in: u64,
+    prev_out: u64,
+}
 
 fn bytes_to_gb(bytes: u64) -> f64 {
     bytes as f64 / 1024.0f64.powi(3)
@@ -10,9 +16,22 @@ fn main() {
     println!("--- Starting sys monitor ---");
 
     let mut sys = System::new_all();
+    let mut net = Networks::new_with_refreshed_list();
+    let mut net_history: HashMap<String, NetworkState> = HashMap::new();
+
+    for (name, data) in &net {
+        net_history.insert(
+            name.clone(),
+            NetworkState {
+                prev_in: data.received(),
+                prev_out: data.transmitted(),
+            },
+        );
+    }
 
     loop {
         sys.refresh_all();
+        net.refresh(false);
 
         print!("{}[2J", 27 as char);
 
@@ -30,6 +49,33 @@ fn main() {
         println!("\nCPUs");
         for (i, cpu) in sys.cpus().iter().enumerate() {
             println!("   CPU {}: {:.2}%", i, cpu.cpu_usage());
+        }
+
+        // Network
+        println!("\nNetworking");
+        for (interface_name, data) in &net {
+            let curr_in = data.received();
+            let curr_out = data.transmitted();
+
+            let stats = net_history
+                .entry(interface_name.clone())
+                .or_insert(NetworkState {
+                    prev_in: curr_in,
+                    prev_out: curr_out,
+                });
+
+            let diff_in = curr_in.saturating_sub(stats.prev_in);
+            let diff_out = curr_out.saturating_sub(stats.prev_out);
+
+            stats.prev_in = curr_in;
+            stats.prev_out = curr_out;
+
+            println!(
+                "  {}: ⬇️  {:.2} KB/s | ⬆️  {:.2} KB/s",
+                interface_name,
+                diff_in as f64 / 1024.0,
+                diff_out as f64 / 1024.0
+            );
         }
 
         thread::sleep(Duration::from_secs(1))
